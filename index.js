698d4d10c7ec9ed228f9c266ec73441237376467
DiegoOTdC <studioartdiego@gmail.com>
2020-06-25 18:32:20 +0200

    Created server with welcome message

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+
+const app = express()
+
+function render (message) {
+    const document = `<html>
+        <head>
+            <title>Welcome/Home</title>
+        </head>
+        <body>
+            <h1>Welcome ${message}</h1>
+        </body>
+    </html>`
+    return document
+}
+
+
+
+app.get(
+    '/user/:name',
+    (request, response) => {
+        const message = `${request.params.name}` 
+        const page = render(message)
+        response.send(page)
+    }
+)
+
+const port = 3000
+
+app.listen(
+    port,
+    () => console.log(`Listening on :${port}`)
+)
